ac4f2198fbd8d656838a85e30d17f04f6f01901e
suren <suren@localhost.localdomain>
2019-02-13 07:24:53 +0545

    adding

--- start.py
+++ start.py
@@ -5,10 +5,16 @@ This is show me the file what i need
 """
 import wordlist as wordlist
 def search(word):
-    print(word.upper())
-    print(wordlist.wordlist[word.upper()])
+    try:
+        print(word,":")
+        print(wordlist.wordlist[word.upper()])
+    except KeyError:
+        print("not found")
+
 
 if __name__ == "__main__":
-    print("welcome find the word")
-    word = input("\n Your word ")
+    print("The meaning I use to read which is personal")
+    print("-"*50)
+    word = input("\n Search the word : ")
+    print("*"*50)
     print(search(word))
