391fd64e13b1c8c264b9ddf54c98955ede747e9f
suren40 <37301538+suren40@users.noreply.github.com>
2019-02-13 07:34:03 +0545

    updated from remote

--- wordlist.py
+++ wordlist.py
@@ -3,5 +3,6 @@
 put the word in capital
 """
 wordlist = { 
-"PYTHON":"This is the programming language,used for multipurpose"
+"PYTHON":"This is the programming language,used for multipurpose",
+"PAYLOAD": {"COMPUTER":"virus","Electrical":"jasle garda hamlai sajilo hunxa"}
 }
